a79a92e5cbd21fabbe1f39842d2513d15e7db3d7
James Pond <james@cipher.host>
2024-07-19 13:47:08 -0300

    Remove featured image from index
    Signed-off-by: James Pond <james@cipher.host>

--- content.php
+++ content.php
@@ -6,36 +6,9 @@
  */
 
 declare ( strict_types = 1 );
-
-$hamilton_extra_classes = 'post-preview tracker';
-
-// Determine whether a fallback is needed for sizing.
-if ( has_post_thumbnail() && ! post_password_required() ) {
-	$hamilton_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'hamilton_preview-image' );
-	if ( $hamilton_image ) {
-		$hamilton_aspect_ratio = $hamilton_image[2] / $hamilton_image[1];
-		// Guaranteee a mininum aspect ratio of 3/4.
-		if ( $hamilton_aspect_ratio <= 0.75 ) {
-			$hamilton_extra_classes .= ' fallback-image';
-		}
-	}
-} else {
-	$hamilton_extra_classes .= ' fallback-image';
-}
-
-$hamilton_image_style_attr = ( has_post_thumbnail() && ! post_password_required() ) ? ' style="background-image: url( ' . esc_url( get_the_post_thumbnail_url( $post->ID, 'hamilton_preview-image' ) ) . ' );"' : '';
-
 ?>
 
-<a <?php post_class( $hamilton_extra_classes ); ?> id="post-<?php the_ID(); ?>" href="<?php the_permalink(); ?>">
-
-	<div class="preview-image"<?php echo esc_attr( $hamilton_image_style_attr ); ?>>
-		<?php
-		if ( has_post_thumbnail() && ! post_password_required() ) {
-			the_post_thumbnail( 'hamilton_preview-image' );
-		}
-		?>
-	</div>
+<a <?php post_class( 'post-preview tracker' ); ?> id="post-<?php the_ID(); ?>" href="<?php the_permalink(); ?>">
 
 	<header class="preview-header">
 
